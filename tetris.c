c0ee31ca46adc91d56a1c50b9e4e471a6b760f72
Juris Panganiban <jurispanganiban@gmail.com>
2026-03-04 23:25:43 +0800

    added basic scoring

--- tetris.c
+++ tetris.c
@@ -2,9 +2,9 @@
 // * Add gravity
 // * Add turning(will take some time) (in pause(learning matrix rotation)
 //	 	- Note a square for the pieces denoting (x, y) as the top left of the piece size and checking will depend on each piece
-// * Line Clear (done)
-// * Scoring
-// * Menu
+// * Difficulty levels
+// * Scoring (done)
+// * Menu (in progress..)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,27 +50,27 @@ void board_init(u8 board[][SIZE_X])
 
 void board_display(u8 board[][SIZE_X], i32 score)
 {
-	printf("\tTetris\n========================\n");
+	fputs("\tTetris\n========================\n", stdout);
 	for(i32 i = 0; i < SIZE_Y; i++)
 	{
-		printf("||");
+		fputs("||", stdout);
 		for(i32 j = 0; j < SIZE_X; j++)
 		{
 			switch(board[i][j])
 			{
 				case 2:
 				{
-					printf("[]");
+					fputs("[]", stdout);
 				} break;
 
 				case 1:
 				{
-					printf("[]");
+					fputs("[]", stdout);
 				} break;
 
 				default:
 				{
-					printf("  ");
+					fputs("  ", stdout);
 				} break;
 			}
 		}
@@ -81,11 +81,11 @@ void board_display(u8 board[][SIZE_X], i32 score)
 		}
 		else
 		{
-			printf("||\n");
+			fputs("||\n", stdout);
 		}
 	}
 
-	printf("========================\n");
+	fputs("========================\n", stdout);
 }
 
 void piece_spawn(piece_struct piece, u8 board[][SIZE_X])
@@ -173,8 +173,9 @@ bool bottomout_check(u8 board[][SIZE_X])
 }
 
 
-void line_check(u8 board[][SIZE_X])
+u8 line_check(u8 board[][SIZE_X])
 {
+	u8 counter = 0;
 	for(i32 i = 0; i < SIZE_Y; i++)
 	{
 		if(board[i][0] == 1)
@@ -197,9 +198,11 @@ void line_check(u8 board[][SIZE_X])
 						board[k][l] = board[k-1][l];
 					}
 				}
+				counter++;
 			}
 		}
 	}
+	return counter;
 }
 
 void game_start()
@@ -271,7 +274,8 @@ void game_start()
 					}
 				}
 
-				line_check(board);
+				u8 line_cleared = line_check(board);
+				score += (line_cleared * 1000);
 
 				//Spawns a new piece
 				piece_spawn(L, board);
@@ -279,7 +283,72 @@ void game_start()
 
 			printf("You pressed %c\n", input);
 
-			printf("\033[H");
+			fputs("\033[H", stdout);
+		}
+	}
+}
+
+i32 menu(void)
+{
+	system("clear");
+
+	//dummy board for menu
+	u8 board[20][10];
+	board_init(board);
+
+	board_display(board, 0);
+	fputs("\033[H", stdout);
+	fputs("\033[15B\033[27C Controls: W - Turn piece", stdout);
+	fputs("\033[1B\033[15D A - Move left", stdout);
+	fputs("\033[1B\033[14D S - Move down", stdout);
+	fputs("\033[1B\033[14D D - Move right", stdout);
+	fputs("\033[13A\033[49D Press z to enter", stdout);
+	fputs("\033[H", stdout);
+	fputs("\033[6B\033[4C  [START GAME]", stdout);
+	fputs("\033[1B\033[16D        QUIT", stdout);
+	fputs("\033[16B", stdout);
+	fputs("\033[H", stdout);
+
+	u8 choice = 0;
+	while(true)
+	{
+		u8 input;
+		if(input == 'w')
+		{
+			fputs("\033[H", stdout);
+			printf("\033[6B\033[4C   START GAME ");
+			printf("\033[1B\033[16D       [QUIT]");
+			printf("\033[16B");
+		}
+		if(input == 's')
+		{
+					fputs("\033[H", stdout);
+					printf("\033[6B\033[4C  [START GAME]");
+					printf("\033[1B\033[16D        QUIT ");
+					printf("\033[16B");
+		}
+
+		ssize_t n = read(STDIN_FILENO, &input, 1);
+		if(n > 0)
+		{
+			switch(input)
+			{
+				case 'w':
+				{
+					choice = 1;
+					printf("w");
+				} break;
+
+				case 's':
+				{
+					choice = 2;
+				} break;
+
+				case 'z':
+				{
+					return choice;
+				} break;
+			}
 		}
 	}
 }
@@ -295,16 +364,18 @@ int main(void)
 
 	tcsetattr(STDIN_FILENO, TCSANOW, &tetris_termios);
 
-	// board init
+	u8 user_choice = menu();
+	switch(user_choice)
+	{
+		case 1:
+		{
+			system("clear");
+//			game_start();
+		} break;
 
-	system("clear");
-	printf("\033[15B\033[27C Controls: W - Turn piece");
-	printf("\033[1B\033[15D A - Move left");
-	printf("\033[1B\033[14D S - Move down");
-	printf("\033[1B\033[14D D - Move right");
-	printf("\033[H");
+	}
 
-	game_start();
+
 
 	tcsetattr(STDIN_FILENO, TCSANOW, &original_termios);
 	
